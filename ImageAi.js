06f145f329c5138a1df6f95834616891b18e704b
MalikaLaouiti <laouiti.malika@yahoo.com>
2024-07-14 15:21:15 +0200

    adding drop drag div

--- ImageAi.js
+++ ImageAi.js
@@ -1,16 +1,69 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const dropzone = document.getElementById('dropzone');
+    const imageInput = document.getElementById('imageInput');
+    const selectedImage = document.getElementById('selectedImage');
+
+    function removeInfoText() {
+        const infoText = dropzone.querySelector('h3');
+        if (infoText) {
+            infoText.remove();
+        }
+    }
+
+    dropzone.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        dropzone.classList.add('drag-over');
+    });
+
+    dropzone.addEventListener('dragleave', () => {
+        dropzone.classList.remove('drag-over');
+    });
+
+    dropzone.addEventListener('drop', (e) => {
+        e.preventDefault();
+        dropzone.classList.remove('drag-over');
+        removeInfoText();
+        const files = e.dataTransfer.files;
+        if (files.length > 0 && (files[0].type === 'image/png' || files[0].type === 'image/jpeg')) {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                selectedImage.src = event.target.result;
+                selectedImage.style.width="100%";
+                removeInfoText(); // Ensure removal after setting src
+            }
+            reader.readAsDataURL(files[0]);
+        }
+    });
+
+    imageInput.addEventListener('change', (event) => {
+        removeInfoText();
+        previewImage(event);
+    });
+});
+
 function previewImage(event) {
     const reader = new FileReader();
     reader.onload = function () {
         const output = document.getElementById('selectedImage');
+        output.style.width = "100%";
         output.src = reader.result;
-        output.style.display = 'block';
+        removeInfoText(); // Ensure removal after setting src
     }
     reader.readAsDataURL(event.target.files[0]);
 }
 
 function resetImage() {
     const output = document.getElementById('selectedImage');
-    output.src = '#';
-    output.style.display = 'none';
+    output.src = 'image.png';
+    output.style.width = "20%";
     document.getElementById('imageInput').value = "";
+
+    const dropzone = document.getElementById('dropzone');
+    dropzone.classList.remove('drag-over');
+
+    if (!dropzone.querySelector('h3')) {
+        const infoText = document.createElement('h3');
+        infoText.textContent = 'drag & drop your PNG or JPEG files here';
+        dropzone.appendChild(infoText);
+    }
 }
