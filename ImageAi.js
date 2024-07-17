d33c1aed354bb000ee0e9517029a7d6848151f38
MalikaLaouiti <laouiti.malika@yahoo.com>
2024-07-17 10:41:06 +0200

    ajouter azure package

--- ImageAi.js
+++ ImageAi.js
@@ -1,3 +1,25 @@
+
+const { ImageAnalysisClient } = require('@azure-rest/ai-vision-image-analysis');
+const createClient = require('@azure-rest/ai-vision-image-analysis').default;
+const { AzureKeyCredential } = require('@azure/core-auth');
+
+// Load the .env file if it exists
+require("dotenv").config();
+
+const endpoint = process.env['VISION_ENDPOINT'];
+const key = process.env['VISION_KEY'];
+
+const credential = new AzureKeyCredential(key);
+const client = createClient(endpoint, credential);
+
+const features = [
+  'Caption',
+  'Read'
+];
+
+const imageUrl = 'https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png';
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const dropzone = document.getElementById('dropzone');
     const imageInput = document.getElementById('imageInput');
@@ -24,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
         dropzone.classList.remove('drag-over');
         removeInfoText();
         const files = e.dataTransfer.files;
-        if (files.length > 0 && (files[0].type === 'image/png' || files[0].type === 'image/jpeg')) {
+        if (files.length > 0 && (files[0].type === 'image/png' )) {
             const reader = new FileReader();
             reader.onload = function (event) {
                 selectedImage.src = event.target.result;
@@ -67,3 +89,32 @@ function resetImage() {
         dropzone.appendChild(infoText);
     }
 }
+
+async function analyzeImageFromUrl() {
+  const result = await client.path('/imageanalysis:analyze').post({
+    body: {
+        url: imageUrl
+    },
+    queryParameters: {
+        features: features
+    },
+    contentType: 'application/json'
+  });
+
+  const iaResult = result.body;
+
+  let output = '';
+
+  if (iaResult.captionResult) {
+    output += `Caption: ${iaResult.captionResult.text} (confidence: ${iaResult.captionResult.confidence})\n`;
+  }
+  if (iaResult.readResult) {
+    iaResult.readResult.blocks.forEach(block => {
+      output += `Text Block: ${JSON.stringify(block)}\n`;
+    });
+  }
+
+  document.getElementById('output').textContent = output;
+}
+
+document.getElementById('submit-button').addEventListener('click', analyzeImageFromUrl);
